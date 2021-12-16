48fa0e756191699c25b6e8920c9908f2a5f6ec08
German Preciat <gpreciat@192.168.2.7>
2021-12-16 16:53:16 +0100

    test RHMC

---
+++ src/analysis/sampling/testRHMC/testRHMC.m
@@ -0,0 +1,17 @@
+
+clear
+global CBTDIR;
+
+% Select a path
+path = [CBTDIR filesep 'src' filesep 'analysis' filesep 'sampling' filesep 'testRHMC'];
+
+% Load model
+load([path filesep 'modelRHMC.mat'])
+
+optionsSampling.samplerName = 'RHMC';
+optionsSampling.nPointsReturned = 1000;
+[~, S_model] =  sampleCbModel(model, [], optionsSampling.samplerName, optionsSampling);
+
+if isempty(S_model)
+    error('The sampling was not completed')
+end
